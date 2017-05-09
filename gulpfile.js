08706d451786d19189ad9cb27ffbbfbc571e4ff1
arifrhkim <arif.judicious@gmail.com>
2017-05-09 10:23:57 +0700

    Adding template

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,9 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("scss/*.scss", ['sass']);
     gulp.watch("css/*.css").on('change', browserSync.reload);
     gulp.watch("*.html").on('change', browserSync.reload);
+
+    gulp.watch("template/*.html", ['template']);
+
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -19,6 +22,12 @@ gulp.task('sass', function() {
     return gulp.src("scss/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("css/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('template', function() {
+    return gulp.src("template/*.html")
+        .pipe(gulp.dest("template/"))
         .pipe(browserSync.stream());
 });
 
